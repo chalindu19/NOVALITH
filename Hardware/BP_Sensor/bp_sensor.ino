48fa4c01e94cd45a3dba29d37c98b19ba369df08
banuramacbook <banurayomaldesilva@gmail.com>
2025-03-04 12:35:12 +0530

    define global variables

--- Hardware/BP_Sensor/bp_sensor.ino
+++ Hardware/BP_Sensor/bp_sensor.ino
@@ -2,3 +2,9 @@
 #include <Firebase_ESP_Client.h>
 #include "DHT.h"
 
+#define READ_INTERVAL 60000  // Interval to send data to Firebase in milliseconds
+
+unsigned long lastTime = 0;
+float pressure = 0.0;
+float temperature = 0.0; 
+
