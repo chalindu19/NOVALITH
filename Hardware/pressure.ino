03ff3fad8772fbbbcf015009754fa252a74c01e6
Minon616 <162681134+Minon616@users.noreply.github.com>
2025-03-04 10:56:04 +0530

    Added the wifi module 1st.

--- Hardware/pressure.ino
+++ Hardware/pressure.ino
@@ -1,68 +1,19 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
+#ifndef WIFI_SETUP_H
+#define WIFI_SETUP_H
 
-// Define the pin where the FSR402 is connected
-const int fsrPin = A0;  
-int batteryLevel = 100; // simulate the battery percentag
+#include <WiFi.h>
 
+#define WIFI_SSID "Galaxy S9"
+#define WIFI_PASSWORD "839747650"
 
-BLEServer *pServer = nullptr;
-BLEService *pService = nullptr;
-BLECharacteristic *pPressureCharacteristic = nullptr;
-BLECharacteristic *pBatteryCharacteristic = nullptr;
-
-
-void setup() {
-  
-  Serial.begin(115200);
-
-  // Initialize BLE
-  BLEDevice::init("FSR402_Sensor");  
-  pServer = BLEDevice::createServer();
-  pService = pServer->createService(BLEUUID((uint16_t)0x180D)); 
-
-  pPressureCharacteristic = pService->createCharacteristic(
-                      BLEUUID((uint16_t)0x2A1C),   
-                      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                    );
-
-  pBatteryCharacteristic = pService -> createCharacteristic(
-                              BLEUUID((uint16_t)0x2A1C),   
-                              BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY              
-  );                 
-
-  pPressureCharacteristic->addDescriptor(new BLE2902());
-  pBatteryCharacteristic->addDescriptor(new BLE2902());
-
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-
-  Serial.println("BLE server is running, with Battery level waiting for connections...");
+void initWiFi() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  
-  int fsrReading = analogRead(fsrPin);
-  String pressure = String(fsrReading);
-  String battery = String(batteryLevel) + "%";
-
-
-  
-  pPressureCharacteristic->setValue(pressure.c_str());
-  pPressureCharacteristic->notify();
-
-  pBatteryCharacteristic->setValue(pressure.c_str());
-  pBatteryCharacteristic->notify();
-
-  
-  Serial.print("FSR Reading: ");
-  Serial.println(fsrReading);
-  Serial.print("  | Battery: ");
-  Serial.println(battery);
-
-  batteryLevel = max(0,batteryLevel - 1);
-  delay(500);  // Update every 500ms
-}
+#endif
