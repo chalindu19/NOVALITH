4a350de3f782a97a8996ecfadb44e59a34a3821f
banuramacbook <banurayomaldesilva@gmail.com>
2025-03-05 18:30:07 +0530

    implement wifi connection setup

--- Hardware/BP_Sensor/bp_sensor.ino
+++ Hardware/BP_Sensor/bp_sensor.ino
@@ -7,4 +7,14 @@
 unsigned long lastTime = 0;
 float pressure = 0.0;
 float temperature = 0.0; 
+void initWiFi() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+    Serial.println("Connected! IP Address: ");
+    Serial.println(WiFi.localIP());
+  }  
 
