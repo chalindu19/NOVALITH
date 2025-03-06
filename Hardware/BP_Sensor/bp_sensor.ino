e64e32567148d7379c0383400cd1432f84fe4ea7
banuramacbook <banurayomaldesilva@gmail.com>
2025-03-06 14:42:21 +0530

    define the pressure sensor pins

--- Hardware/BP_Sensor/bp_sensor.ino
+++ Hardware/BP_Sensor/bp_sensor.ino
@@ -2,19 +2,14 @@
 #include <Firebase_ESP_Client.h>
 #include "DHT.h"
 
+//wifi and fire base set up 
+
+
+
+
+#define DOUT_Pin 17
+#define SCK_Pin 16 
 #define READ_INTERVAL 60000  // Interval to send data to Firebase in milliseconds
 
-unsigned long lastTime = 0;
-float pressure = 0.0;
-float temperature = 0.0; 
-void initWiFi() {
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to WiFi ..");
-    while (WiFi.status() != WL_CONNECTED) {
-      Serial.print('.');
-      delay(1000);
-    }
-    Serial.println("Connected! IP Address: ");
-    Serial.println(WiFi.localIP());
-  }  
+
 
