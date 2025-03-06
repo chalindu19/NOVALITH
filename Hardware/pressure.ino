926d83f11c444f1efcc3588c65bd311e45e9ac57
Minon616 <162681134+Minon616@users.noreply.github.com>
2025-03-06 10:40:06 +0530

    Included the network setup

--- Hardware/pressure.ino
+++ Hardware/pressure.ino
@@ -13,50 +13,54 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 void tokenStatusCallback(TokenInfo info) {
-  Serial.printf("Token Info: type = %s, status = %s\n", 
-                info.token_type.c_str(), 
-                info.status.c_str());
+    Serial.printf("Token Info: type = %s, status = %s\n", 
+                  info.token_type.c_str(), 
+                  info.status.c_str());
 }
 
 void initWiFi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println("\nConnected to WiFi!");
-  Serial.println(WiFi.localIP());
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.println("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println("\nConnected to WiFi!");
+    Serial.println(WiFi.localIP());
+}
+
+void initializeNetwork() {
+    initWiFi();
+
+    // Assign Firebase credentials
+    config.api_key = API_KEY;
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+    config.database_url = DATABASE_URL;
+
+    Firebase.reconnectWiFi(true);
+    fbdo.setResponseSize(4096);
+    config.token_status_callback = tokenStatusCallback;
+    config.max_token_generation_retry = 5;
+
+    // Initialize Firebase
+    Firebase.begin(&config, &auth);
+
+    Serial.println("Getting User UID...");
+    while (auth.token.uid == "") {
+        Serial.print('.');
+        delay(1000);
+    }
+
+    Serial.print("\nUser UID: ");
+    Serial.println(auth.token.uid.c_str());
 }
 
 void setup() {
-  Serial.begin(9600);
-  initWiFi();
-
-  // Assign Firebase credentials
-  config.api_key = API_KEY;
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-  config.database_url = DATABASE_URL;
-
-  Firebase.reconnectWiFi(true);
-  fbdo.setResponseSize(4096);
-  config.token_status_callback = tokenStatusCallback;
-  config.max_token_generation_retry = 5;
-
-  // Initialize Firebase
-  Firebase.begin(&config, &auth);
-
-  Serial.println("Getting User UID...");
-  while (auth.token.uid == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-
-  Serial.print("\nUser UID: ");
-  Serial.println(auth.token.uid.c_str());
+    Serial.begin(115200);
+    initializeNetwork(); // Connects to WiFi and Firebase
 }
 
 void loop() {
-  // Add sensor reading logic here
+    // Sensor data processing code will be added here
 }
