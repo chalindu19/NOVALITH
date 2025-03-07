6416c1fbfb345198052ad964af997f455119b16d
sandun <sadunanjana2003@gmail.com>
2025-03-07 15:44:17 +0530

    Added required imports for Forget Password screen

--- Software/novalith/lib/views/Auth/forget_password.dart
+++ Software/novalith/lib/views/Auth/forget_password.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:form_validation/form_validation.dart';
+import 'package:novalith/Controllers/AuthController.dart';
+import 'package:novalith/Models/Strings/forget_password.dart';
+import 'package:novalith/Models/Strings/register_screen.dart';
+import 'package:novalith/Models/Utils/Colors.dart';
+import 'package:novalith/Models/Utils/Common.dart';
+import 'package:novalith/Models/Utils/Images.dart';
+import 'package:novalith/Models/Utils/Routes.dart';
+import 'package:novalith/Models/Utils/Utils.dart';
+import 'package:novalith/Views/Widgets/custom_button.dart';
+import 'package:novalith/Views/Widgets/custom_back_button.dart';
+import 'package:novalith/Views/Widgets/custom_text_form_field.dart';
 
 
 
