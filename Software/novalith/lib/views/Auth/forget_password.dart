9cb2f51124985e6796f1fe7ab899943a6b51a3cd
sandun <sadunanjana2003@gmail.com>
2025-03-07 15:40:06 +0530

    Added Forget Password screen structure

--- Software/novalith/lib/views/Auth/forget_password.dart
+++ Software/novalith/lib/views/Auth/forget_password.dart
@@ -1,5 +1,23 @@
+import 'package:flutter/material.dart';
 
 
-class ForgetPassword  {
 
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  State<ForgetPassword> createState() => _LoginState();
+}
+
+class _LoginState extends State<ForgetPassword> {
+  final AuthController _authController = AuthController();
+  final _keyForm = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+
+  dynamic response;
+
+  @override
+  void initState() {
+    super.initState();
   }
