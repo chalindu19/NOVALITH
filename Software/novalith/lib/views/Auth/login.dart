f14726c3c2b1e2db3a2a4a73f443c74e2a2f3a8e
sandun <sadunanjana2003@gmail.com>
2025-03-07 18:02:30 +0530

    Added required imports for Login screen

--- Software/novalith/lib/views/Auth/login.dart
+++ Software/novalith/lib/views/Auth/login.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:form_validation/form_validation.dart';
+import 'package:novalith/Controllers/AuthController.dart';
+import 'package:novalith/Models/Strings/login_screen.dart';
+import 'package:novalith/Models/Strings/register_screen.dart';
+import 'package:novalith/Models/Utils/Colors.dart';
+import 'package:novalith/Models/Utils/Common.dart';
+import 'package:novalith/Models/Utils/Images.dart';
+import 'package:novalith/Models/Utils/Routes.dart';
+import 'package:novalith/Models/Utils/Utils.dart';
+import 'package:novalith/Views/Auth/forget_password.dart';
+import 'package:novalith/Views/Auth/register.dart';
+import 'package:novalith/Views/Contents/Home/home.dart';
+import 'package:novalith/Views/Widgets/custom_button.dart';
+import 'package:novalith/Views/Widgets/custom_text_form_field.dart';
+
 
 class Login extends StatefulWidget {
   const Login({super.key});
