fdf1400899166bcca07577c988b4b85af3c1f1a2
sandun <sadunanjana2003@gmail.com>
2025-03-08 13:12:36 +0530

    Implement UI layout for CustomDropDown

--- Software/novalith/lib/views/Widgets/custom_dropdown.dart
+++ Software/novalith/lib/views/Widgets/custom_dropdown.dart
@@ -27,4 +27,64 @@ class CustomDropDown extends StatelessWidget {
     required this.function,
     required this.items,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        (label != null)
+            ? Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: Text(
+                  label,
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      color: color11),
+                ),
+              )
+            : const SizedBox.shrink(),
+        Container(
+            decoration: BoxDecoration(
+                color: background_color,
+                borderRadius: BorderRadius.circular(5.0)),
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: Row(
+              children: [
+                (leading_image != '')
+                    ? Expanded(flex: 0, child: Image.asset(leading_image))
+                    : const SizedBox.shrink(),
+                (leading_icon != null)
+                    ? Expanded(
+                        flex: 0,
+                        child: Icon(
+                          leading_icon,
+                          color: leading_icon_color,
+                        ))
+                    : const SizedBox.shrink(),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: dropdown_value,
+                        dropdownColor: color6,
+                        elevation: 5,
+                        style: TextStyle(color: color8),
+                        underline: Container(
+                          height: 2,
+                          color: underline_color,
+                        ),
+                        onChanged: function,
+                        items: items),
+                  ),
+                )
+              ],
+            ))
+      ],
+    );
+  }
 }
+
