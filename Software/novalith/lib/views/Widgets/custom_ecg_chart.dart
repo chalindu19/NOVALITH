b258df9a78d43d4bf36e89c665a6abff8848e815
sandun <sadunanjana2003@gmail.com>
2025-03-08 14:58:55 +0530

    Implement ECGChart UI using Syncfusion Chart

--- Software/novalith/lib/views/Widgets/custom_ecg_chart.dart
+++ Software/novalith/lib/views/Widgets/custom_ecg_chart.dart
@@ -15,4 +15,27 @@ class _ECGChartState extends State<ECGChart> {
   void initState() {
     super.initState();
   }
+
+ @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      title: ChartTitle(text: 'ECG Graph'),
+      primaryXAxis: NumericAxis(
+        majorGridLines: MajorGridLines(width: 0), // Hide X-axis grid lines
+      ),
+      primaryYAxis: NumericAxis(
+        majorGridLines: MajorGridLines(width: 0), // Hide Y-axis grid lines
+      ),
+      series: <CartesianSeries<ChartData, double>>[
+        // Renders line chart
+        LineSeries<ChartData, double>(
+          dataSource: widget.chartData,
+          xValueMapper: (ChartData data, _) => data.x,
+          yValueMapper: (ChartData data, _) => data.y,
+          color: Colors.red,
+          width: 2,
+        ),
+      ],
+    );
+  }
 }
