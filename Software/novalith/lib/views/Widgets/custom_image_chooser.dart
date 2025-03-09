0ccd10be64124bc606c00780b20e630bc8805d55
sandun <sadunanjana2003@gmail.com>
2025-03-09 14:42:23 +0530

    Display selected images in CustomImageChooser

--- Software/novalith/lib/views/Widgets/custom_image_chooser.dart
+++ Software/novalith/lib/views/Widgets/custom_image_chooser.dart
@@ -56,4 +56,57 @@ class CustomImageChooser extends StatelessWidget {
       ],
     );
   }
-}
+
+ getContents(context) {
+    List<Widget> widgetList = [];
+
+    for (var i = 0; i < images.length; i++) {
+      Uint8List image = images[i];
+
+      widgetList.add(Card(
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                  flex: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: SizedBox(
+                      height: displaySize.width * 0.2,
+                      width: displaySize.width * 0.2,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.memory(
+                          image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Text(
+                      "${(image.lengthInBytes / 1000000).toStringAsFixed(2)} MB")),
+              Expanded(
+                  flex: 0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            onItemRemove(i);
+                          },
+                          child: Icon(Icons.close, color: color13),
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ));
+    }
