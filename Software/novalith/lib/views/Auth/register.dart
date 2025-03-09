a2ab00d439658cdb1a1981cd622ce4407011a9a6
sandun <sadunanjana2003@gmail.com>
2025-03-09 23:16:09 +0530

    Define BusinessRegister class with basic UI

---
+++ Software/novalith/lib/views/Auth/register.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BusinessRegister extends StatefulWidget {
+  const BusinessRegister({super.key});
+
+  @override
+  State<BusinessRegister> createState() => _BusinessRegisterState();
+}
+
+class _BusinessRegisterState extends State<BusinessRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(""),
+      ),
+    );
+  }
+}
