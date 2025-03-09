64a7cdfec93c090c309537531a77167336e2f3ee
sandun <sadunanjana2003@gmail.com>
2025-03-09 22:33:31 +0530

    Implement UI for CustomSwitchButton with CupertinoSwitch

--- Software/novalith/lib/views/Widgets/custom_switch_button.dart
+++ Software/novalith/lib/views/Widgets/custom_switch_button.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:novalith/Models/Utils/Colors.dart';
 
 class CustomSwitchButton extends StatelessWidget {
-  final Function(bool)? onChanged;
-  final bool switchValue;
+  Function(bool)? onChanged;
+  bool switchValue;
 
   CustomSwitchButton({Key? key, this.onChanged, this.switchValue = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(); // Placeholder for UI
+    return CupertinoSwitch(
+      value: switchValue,
+      activeColor: colorPrimary,
+      onChanged: onChanged,
+    );
   }
 }
