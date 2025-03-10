12a56e68b6a08b53a30de0b759308b9934b22b76
Chalindu Yohan <chalinduyohan19@gmail.com>
2025-03-10 10:17:57 +0530

    Enabled power-saving mode for ESP32 during idle.

--- Hardware/ECG_Monitoring/ECG_AD8232.ino
+++ Hardware/ECG_Monitoring/ECG_AD8232.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
+#include <esp_sleep.h>
 
 #define AD8232_SIGNAL A0  
 #define LO_PLUS 2         
@@ -7,18 +8,18 @@
 #define ECG_THRESHOLD 550  
 #define SPIKE_THRESHOLD 150  
 #define PEAK_DELAY 200  
+#define WAKE_UP_PIN 34  
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 9, 8);
-
 unsigned long lastPeakTime = 0;
-int lastECGValue = 0;
 
 void setup() {
     Serial.begin(115200);
     pinMode(AD8232_SIGNAL, INPUT);
     pinMode(LO_PLUS, INPUT);
     pinMode(LO_MINUS, INPUT);
-
+    pinMode(WAKE_UP_PIN, INPUT_PULLUP);
+    
     tft.begin();
     tft.setRotation(3);
     tft.fillScreen(ILI9341_BLACK);
@@ -33,21 +34,18 @@ void loop() {
     Serial.print("ECG: ");
     Serial.println(ecgValue);
 
-    // Display ECG value
     tft.fillRect(10, 30, 200, 20, ILI9341_BLACK);
     tft.setCursor(10, 30);
     tft.print("ECG: ");
     tft.print(ecgValue);
 
-    // Detect heart rate
-    if (ecgValue > ECG_THRESHOLD && (ecgValue - lastECGValue) > SPIKE_THRESHOLD) {
+    if (ecgValue > ECG_THRESHOLD) {
         unsigned long currentTime = millis();
         if (currentTime - lastPeakTime > PEAK_DELAY) {
             int heartRate = 60000 / (currentTime - lastPeakTime);
             Serial.print("HR: ");
             Serial.println(heartRate);
 
-            // Display heart rate
             tft.fillRect(10, 50, 200, 20, ILI9341_BLACK);
             tft.setCursor(10, 50);
             tft.print("HR: ");
@@ -58,7 +56,6 @@ void loop() {
         }
     }
 
-    // Leads-off detection
     if (digitalRead(LO_PLUS) || digitalRead(LO_MINUS)) {
         Serial.println("WARNING: Leads Off!");
         tft.fillRect(10, 70, 200, 20, ILI9341_BLACK);
@@ -70,5 +67,9 @@ void loop() {
         tft.fillRect(10, 70, 200, 20, ILI9341_BLACK);
     }
 
-    lastECGValue = ecgValue;
+    if (ecgValue < ECG_THRESHOLD) {
+        Serial.println("Entering deep sleep...");
+        esp_sleep_enable_ext0_wakeup((gpio_num_t)WAKE_UP_PIN, HIGH);
+        esp_deep_sleep_start();
+    }
 }
