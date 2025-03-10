f8f854b281afbf3ad41de1b0b384d6d3de8bf9f9
Chalindu Yohan <chalinduyohan19@gmail.com>
2025-03-10 22:22:24 +0530

    Removed the unwanted Software files.

--- Software/novalith/lib/Models/Strings/app.dart
+++
@@ -1,4 +0,0 @@
-const String app_name = 'Novalith';
-const String app_quote = 'Track Your Baby’s Growth';
-const String app_something_wrong = 'Something wrong.';
-const String app_processing = 'Processing';
