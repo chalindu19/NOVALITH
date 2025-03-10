77cbefb704bdd7e9ff76e3e440c473a96d66333e
sandun <sadunanjana2003@gmail.com>
2025-03-10 11:08:35 +0530

    Add form fields and controllers in BusinessRegister screen

--- Software/novalith/lib/views/Auth/register.dart
+++ Software/novalith/lib/views/Auth/register.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:form_validation/form_validation.dart';
+import 'package:novalith/Controllers/AuthController.dart';
+import 'package:novalith/Models/DB/LoggedUser.dart';
+import 'package:novalith/Models/Strings/main_screen.dart';
+import 'package:novalith/Models/Utils/Common.dart';
+import 'package:novalith/Models/Utils/Utils.dart';
+import 'package:novalith/Views/Widgets/custom_button.dart';
+import 'package:novalith/Views/Widgets/custom_text_form_field.dart';
+
+import '../../Models/Strings/register_screen.dart';
+import '../../Models/Utils/Colors.dart';
+import '../../Models/Utils/Routes.dart';
+import '../Widgets/custom_back_button.dart';
 
 class BusinessRegister extends StatefulWidget {
   const BusinessRegister({super.key});
@@ -8,12 +21,27 @@ class BusinessRegister extends StatefulWidget {
 }
 
 class _BusinessRegisterState extends State<BusinessRegister> {
+  bool termsAndConditionCheck = false;
+
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _mobile = TextEditingController();
+  final TextEditingController _address = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirm_password = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  final AuthController _authController = AuthController();
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(""),
-      ),
-    );
+  void initState() {
+    _name.text = "";
+    _mobile.text = "";
+    _address.text = "";
+    _email.text = "";
+    _password.text = "";
+    _confirm_password.text = "";
+
+    super.initState();
   }
-}
